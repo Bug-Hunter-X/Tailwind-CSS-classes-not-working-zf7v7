47529fc3cc73e18a6bd4d260df3bab29e67726b2
BugHunter-X <askzenai@gmail.com>
2025-01-15 08:42:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+```javascript
+//In your main app.js file
+import 'tailwindcss/tailwind.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200">
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+```
